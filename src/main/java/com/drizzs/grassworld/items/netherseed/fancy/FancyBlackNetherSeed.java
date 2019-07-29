a6751ac65ed5a74ad715a328b1ac1678070b17fa
Drizzs <drizzs@hotmail.com>
2019-07-29 02:27:42 -0400

    Added Nether and End Seeds
    Prepping for Update, Added Seeds

---
+++ src/main/java/com/drizzs/grassworld/items/netherseed/fancy/FancyBlackNetherSeed.java
@@ -0,0 +1,47 @@
+package com.drizzs.grassworld.items.netherseed.normal;
+
+import com.drizzs.grassworld.blocks.GrassWorldBlocks;
+import com.drizzs.grassworld.items.GrassWorldItems;
+import com.drizzs.grassworld.util.ItemGroupGW;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+
+public class BlackNetherSeed extends Item {
+
+    public BlackNetherSeed(Properties group)
+    {
+        super(new Properties().group(ItemGroupGW.instance).maxStackSize(16));
+
+    }
+
+    @Nonnull
+    public ActionResultType onItemUse(ItemUseContext context) {
+        World world = context.getWorld();
+        BlockPos blockpos = context.getPos();
+        ItemStack item = context.getItem();
+        Hand hand = context.getHand();
+
+        if (!(item.getItem() == GrassWorldItems.blacknetherseed)){
+
+            return ActionResultType.PASS;
+        }
+
+        if (world.getBlockState(blockpos).getBlock() == Blocks.NETHERRACK)
+        {
+
+            world.setBlockState(blockpos, GrassWorldBlocks.blacknethergrass.getDefaultState());
+
+        }
+        return ActionResultType.SUCCESS;
+
+    }
+
+}
