45bd4371963570531280ef5e54f356d28f4df00e
Drizzs <drizzs@hotmail.com>
2019-09-28 18:51:22 -0400

    same as last

--- src/main/java/com/drizzs/grassworld/particle/GrassParticleRegistry.java
+++ src/main/java/com/drizzs/grassworld/particle/GrassParticleRegistry.java
@@ -1,4 +1,42 @@
 package com.drizzs.grassworld.particle;
 
-public class GrassParticles {
+
+import com.drizzs.grassworld.GrassWorld;
+import com.drizzs.grassworld.particle.types.ShimmerParticleData;
+import com.drizzs.grassworld.particle.types.ShimmerParticleType;
+import net.minecraft.particles.ParticleType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+import net.minecraftforge.registries.ObjectHolder;
+
+@Mod.EventBusSubscriber(modid = GrassWorld.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class GrassParticleRegistry
+{
+
+    @ObjectHolder("grassworld:shimmer")
+    public static ParticleType<ShimmerParticleData> SHIMMER;
+
+    @SubscribeEvent
+    public static void registerParticles(RegistryEvent.Register<ParticleType<?>> event) {
+        register(event.getRegistry(), new ShimmerParticleType(), "shimmer");
+    }
+
+    public static <V extends IForgeRegistryEntry<V>> void register(IForgeRegistry<V> reg, IForgeRegistryEntry<V> thing, ResourceLocation name) {
+        reg.register(thing.setRegistryName(name));
+    }
+
+    public static <V extends IForgeRegistryEntry<V>> void register(IForgeRegistry<V> reg, IForgeRegistryEntry<V> thing, String name) {
+        register(reg, thing, new ResourceLocation(GrassWorld.MOD_ID, name));
+    }
+
+
+
+
+
+
+
 }
--- src/main/java/com/drizzs/grassworld/util/config/Config.java
+++ src/main/java/com/drizzs/grassworld/util/config/Config.java
@@ -1,4 +1,29 @@
 package com.drizzs.grassworld.util.config;
 
-public class Config {
+import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.commons.lang3.tuple.Pair;
+
+public final class Config {
+
+    public static class Client {
+        public final ForgeConfigSpec.BooleanValue particleOn;
+
+
+
+        public Client(ForgeConfigSpec.Builder builder) {
+
+            particleOn = builder
+                    .comment("Set this to true if you're not running a server in your mom's basement for 200 people!")
+                    .define("particleOn", true);
+        }
+
+    }
+
+    public static final Client CLIENT;
+    public static final ForgeConfigSpec CLIENT_SPEC;
+    static {
+        final Pair<Client, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Client::new);
+        CLIENT_SPEC = specPair.getRight();
+        CLIENT = specPair.getLeft();
+    }
 }
